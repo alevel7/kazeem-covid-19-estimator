0bb4a4e95e029c2e0bf83f434db6512ec707583a
kazeem taiwo <alevel7@gmail.com>
2020-04-11 18:51:06 +0100

    new updates

--- src/estimator.js
+++ src/estimator.js
@@ -70,18 +70,18 @@ const covid19ImpactEstimator = (data) => {
 };
 
 
-const data = {
-  region: {
-    name: 'Africa',
-    avgAge: 19.7,
-    avgDailyIncomeInUSD: 5,
-    avgDailyIncomePopulation: 0.71
-  },
-  periodType: 'days',
-  timeToElapse: 58,
-  reportedCases: 674,
-  population: 66622705,
-  totalHospitalBeds: 1380614
-};
-console.log(covid19ImpactEstimator(data));
+// const data = {
+//   region: {
+//     name: 'Africa',
+//     avgAge: 19.7,
+//     avgDailyIncomeInUSD: 5,
+//     avgDailyIncomePopulation: 0.71
+//   },
+//   periodType: 'days',
+//   timeToElapse: 58,
+//   reportedCases: 674,
+//   population: 66622705,
+//   totalHospitalBeds: 1380614
+// };
+// console.log(covid19ImpactEstimator(data));
 export default covid19ImpactEstimator;
