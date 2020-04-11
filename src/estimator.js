9d51adc3ff76dff2cc7146b7345efa51c2d5d1d6
kazeem taiwo <alevel7@gmail.com>
2020-04-11 13:58:22 +0100

    new updates added

--- src/estimator.js
+++ src/estimator.js
@@ -57,26 +57,26 @@ const covid19ImpactEstimator = (data) => {
 
   // dollars in flight
   result.impact.dollarsInFlight = Math.trunc((result.impact.infectionsByRequestedTime
-  * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) / days);
+  * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) * days);
   result.severeImpact.dollarsInFlight = Math.trunc((result.severeImpact.infectionsByRequestedTime
-  * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) / days);
+  * data.region.avgDailyIncomePopulation * data.region.avgDailyIncomeInUSD) * days);
 
   return result;
 };
 
 
-// const data = {
-//   region: {
-//     name: 'Africa',
-//     avgAge: 19.7,
-//     avgDailyIncomeInUSD: 5,
-//     avgDailyIncomePopulation: 0.71
-//   },
-//   periodType: 'days',
-//   timeToElapse: 58,
-//   reportedCases: 674,
-//   population: 66622705,
-//   totalHospitalBeds: 1380614
-// };
-// console.log(covid19ImpactEstimator(data));
+const data = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
+};
+console.log(covid19ImpactEstimator(data));
 export default covid19ImpactEstimator;
